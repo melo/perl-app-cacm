7c357437b0134aa967bcbf5174c7e0771b1cd17d
Pedro Melo <melo@simplicidade.org>
2009-03-08 15:32:29 +0000

    Added initial skel for App::CMAM, generated with module-starter
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ App-CMAM/lib/App/CMAM.pm
@@ -0,0 +1,107 @@
+package App::CMAM;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+App::CMAM - The great new App::CMAM!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::CMAM;
+
+    my $foo = App::CMAM->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-cmam at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-CMAM>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::CMAM
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-CMAM>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-CMAM>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-CMAM>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-CMAM>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Pedro Melo, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of App::CMAM
